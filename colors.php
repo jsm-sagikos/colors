f1e335904b2c739a190641958164ad4069763c18
jsm-sagikos <93094671+jsm-sagikos@users.noreply.github.com>
2022-02-18 12:49:08 +0100

    found the proper ones

--- colors.php
+++ colors.php
@@ -1,12 +1,23 @@
  <?php
 
+// Original 3 and 4 bit VGA colors
 $black = "#000000";    // 0
-$red = "#ff0000";      // 1
-$green = "#00ff00";    // 2
-$yellow = "#ffff00";   // 3
-$blue = "#0000ff";     // 4
-$magenta = "#ff00ff";  // 5
-$cyan = "#00ffff";     // 6
-$white = "#ffffff";    // 7
+$red = "#AA0000";      // 1
+$green = "#00AA00";    // 2
+$yellow = "#AA5500";   // 3
+$blue = "#0000AA";     // 4
+$magenta = "#AA00AA";  // 5
+$cyan = "#00AAAA";     // 6
+$white = "#AAAAAA";    // 7
+
+// Bright versions
+$bright_black = "#555555";
+$bright_red = "#FF5555";
+$bright_green = "#55FF55";
+$bright_yellow = "#FFFF55";
+$bright_blue = "#5555FF";
+$bright_magenta = "#FF55FF";
+$bright_cyan = "#55FFFF";
+$bright_white = "#FFFFFF";
 
  
