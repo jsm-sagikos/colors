4dff91bdcbe84a43dc8f1718679b0e6d445fcce9
jsm-sagikos <93094671+jsm-sagikos@users.noreply.github.com>
2022-02-18 10:46:52 +0100

    Update colors.php

--- colors.php
+++ colors.php
@@ -1,3 +1,12 @@
  <?php
 
-$black = "#000000";
+$black = "#000000";    // 0
+$red = "#ff0000";      // 1
+$green = "#00ff00";    // 2
+$yellow = "#ffff00";   // 3
+$blue = "#0000ff";     // 4
+$magenta = "#ff00ff";  // 5
+$cyan = "#00ffff";     // 6
+$white = "#ffffff";    / 7
+
+ 
